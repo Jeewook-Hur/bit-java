6515e33d78a60a214d00445600a83de2e12ec0a8
Jeewook Hur <jeewook2001@naver.com>
2020-08-13 10:49:29 +0900

    20200813

--- JavaEx/src/com/javaex/oop/goods/v1/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v1/Goods.java
@@ -13,5 +13,25 @@ public class Goods {
 	// public > protected > default > private
 	private String name;
 	private int price;
+	
+	// Getter // Setter
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		// this : 해당 인스턴스 자신을 가리키는
+		this.name = name;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	// 메서드
+	public void showInfo() {
+		// 내부 필드의 정보를 출력
+		System.out.printf("%s, 가격%원%n" name ,  price);
+	}
 
 }
